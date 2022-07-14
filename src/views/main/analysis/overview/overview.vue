6cbcf1445f63bfaa27ded7c4fb3f7fa6d6f3c5ad
siyu <siyu@stmail.ujs.edu.cn>
2022-07-14 20:03:39 +0800

    提交hotfix

--- src/views/main/analysis/overview/overview.vue
+++ src/views/main/analysis/overview/overview.vue
@@ -3,7 +3,11 @@
     <h1>The CMS Demo</h1>
     <h2>using Vue3 + TS</h2>
     <h2>这里修改了一部分</h2>
+<<<<<<< HEAD
     <h2>kevin开发的一段代码</h2>
+=======
+    <h2>siyu又开发了一段代码</h2>
+>>>>>>> dev-siyu-0714
   </div>
 </template>
 
